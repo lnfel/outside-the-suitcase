82adba566110ec71f3c931f08dd9f795590553b3
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-10 03:21:40 +0800

    [Change] Update fallback page

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-            fallback: '200.html'
+            fallback: '404.html'
         }),
         prerender: {
             handleHttpError: 'ignore'
