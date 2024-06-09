81c1d8c974fdece9eb446765a7287de2f9dda26c
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-10 00:26:38 +0800

    [Feature] Browser compatible google sheets fetch using API key

---
+++ src/lib/google/sheets/index.ts
@@ -0,0 +1,44 @@
+import type { sheets_v4 } from 'googleapis'
+
+export function filterHeaders(headers: Sheet.RaidTitle[]) {
+    return headers.filter((cell) => cell as string !== "")
+}
+
+export function parseSheetData(headers: Sheet.RaidTitle[], valueRanges?: sheets_v4.Schema$ValueRange[]): Sheet.RaidGroup<typeof headers[number]> {
+    return headers?.reduce((accumulator, header, index) => {
+        if (valueRanges?.[index]) {
+            const values: Sheet.Entry[][] = valueRanges[index].values?.reduce((rowAccumulator, row, rowIndex) => {
+                let entry: Record<typeof cellHeaders[number], any> = {}
+                const cellHeaders = [
+                    "Entry Tag",
+                    "Entry Date",
+                    "Username",
+                    "Score",
+                    "Portray",
+                    "Resonance",
+                    "Amplification",
+                    "Name"
+                ]
+                const entryMeta = cellHeaders.slice(0, 4) as string[]
+                const characters = [row.slice(4, 8), row.slice(8, 12), row.slice(12, 16), row.slice(16, 20)].map((characterData, characterIndex) => {
+                    let charaObject: Record<string, string> = {}
+                    charaObject[cellHeaders[4]] = characterData[0]
+                    charaObject[cellHeaders[5]] = characterData[1]
+                    charaObject[cellHeaders[6]] = characterData[2]
+                    charaObject[cellHeaders[7]] = characterData[3]
+                    return charaObject
+                })
+                entryMeta.forEach((meta: string, metaIndex) => {
+                    entry[meta] = row[metaIndex]
+                })
+                entry.characters = characters
+                rowAccumulator.push(entry)
+                return rowAccumulator
+            }, []) ?? []
+
+            valueRanges[index].values = values
+            accumulator[header] = valueRanges[index] as Sheet.RaidData
+        }
+        return accumulator
+    }, {} as Sheet.RaidGroup<Sheet.RaidTitle>)
+}
