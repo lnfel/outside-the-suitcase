b4f02a85589175416912764c11d66417600bb80c
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-10 15:02:05 +0800

    Updates

---
+++ _app/immutable/chunks/stores.BYmWxR8J.js
@@ -0,0 +1,1 @@
+import"./runtime.TsWTqLqS.js";import{d as e}from"./singletons.DMDzbRQV.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
