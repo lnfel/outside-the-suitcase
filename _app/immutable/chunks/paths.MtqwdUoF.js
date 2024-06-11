fa971591ca90ed46ac1c0a8bbe6d81a5cc21b63a
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-11 12:58:35 +0800

    Updates

--- _app/immutable/chunks/paths.MtqwdUoF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_f9n798?.base??"/outside-the-suitcase",e=globalThis.__sveltekit_f9n798?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/stores.CFjyBpgT.js
@@ -0,0 +1,1 @@
+import"./runtime.BfqAf-RT.js";import{d as e}from"./singletons.D2b37S48.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
