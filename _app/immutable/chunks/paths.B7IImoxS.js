5e20b179952777760086221429aa6189458e8ea9
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-17 00:48:24 +0800

    Updates

---
+++ _app/immutable/chunks/paths.B7IImoxS.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1chm12o?.base??"/outside-the-suitcase",e=globalThis.__sveltekit_1chm12o?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/paths.PlV-g5mt.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_114qa0j?.base??"/outside-the-suitcase",a=globalThis.__sveltekit_114qa0j?.assets??s;export{a,s as b};
