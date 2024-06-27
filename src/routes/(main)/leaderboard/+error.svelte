82803bc085aab3ac3047c65cf4eeda20002fd8a6
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-27 15:15:02 +0800

    [Feature] Simple error page when leaderboard raid matcher is not satisfied

---
+++ src/routes/(main)/leaderboard/+error.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { page } from "$app/stores"
+</script>
+
+<h3 class="crimson-text-bold text-tuscany-600 dark:text-tuscany-300 text-5xl px-10 md:px-0">{ $page.status } { $page.error?.message }</h3>
