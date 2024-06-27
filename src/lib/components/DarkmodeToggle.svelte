0f3f6e8da872cd350290bf64cf1d37ef6d296b81
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-27 14:56:20 +0800

    [Experimental] Transition theme toggle svg fill color, quite hard to do

--- src/lib/components/DarkmodeToggle.svelte
+++ src/lib/components/DarkmodeToggle.svelte
@@ -35,12 +35,15 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/theme-toggles@4.10.1/css/classic.min.css">
 </svelte:head>
 
-<button bind:this={toggleButton} onclick={toggle} class="theme-toggle group flex items-center gap-2 text-slate-800 transition-colors delay-500 outline-none {className}" class:theme-toggle--toggled={enabled} type="button" title="Toggle theme" aria-label="Toggle theme">
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="1.5rem" height="1.5rem" fill="currentColor" stroke-linecap="round" class="theme-toggle__classic pointer-events-none group-data-[state=enabled]:text-amber-300" viewBox="0 0 32 32">
+<button bind:this={toggleButton} onclick={toggle} class="theme-toggle group flex items-center gap-2 text-amber-600 dark:text-amber-300 outline-none {className}" class:theme-toggle--toggled={enabled} type="button" title="Toggle theme" aria-label="Toggle theme">
+    <!-- https://codepen.io/cssence/pen/RRNvQX -->
+    <!-- group-data-[state=enabled]:text-amber-600 group-data-[state=enabled]:dark:text-amber-300 -->
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="1.5rem" height="1.5rem" fill="currentColor" stroke-linecap="round" class="theme-toggle__classic pointer-events-none" viewBox="0 0 32 32">
         <clipPath id="theme-toggle__classic__cutout">
             <path d="M0-5h30a1 1 0 0 0 9 13v24H0Z" />
         </clipPath>
         <g clip-path="url(#theme-toggle__classic__cutout)">
+            <!-- class="transition-all duration-200" -->
             <circle cx="16" cy="16" r="9.34" />
             <g stroke="currentColor" stroke-width="1.5">
                 <path d="M16 5.5v-4" />
@@ -54,5 +57,5 @@
             </g>
         </g>
     </svg>
-    <span class="text-slate-800 dark:text-white group-hover:text-tuscany-600 group-focus:text-tuscany-600 md:sr-only">Switch to {theme === 'dark' ? 'light' : 'dark'} mode</span>
+    <span class="text-slate-800 dark:text-white group-hover:text-tuscany-600 group-focus:text-tuscany-600 dark:group-hover:text-tuscany-400 dark:group-focus:text-tuscany-400 md:sr-only">Switch to {theme === 'dark' ? 'light' : 'dark'} mode</span>
 </button>
