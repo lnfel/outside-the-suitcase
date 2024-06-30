d7e04ee4830b52bb0ebb520c8ccb9d238d88316c
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-06-30 19:50:10 +0800

    [Feature] Update manifest.json during postbuild

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,7 +1,11 @@
 import * as path from "path"
 import { fileURLToPath } from "url"
-import { rm, readdir } from 'node:fs/promises'
+import { rm, readdir, writeFile } from 'node:fs/promises'
 import { crayon } from "crayon.js"
+import manifest from "../build/manifest.json" with { type: "json" }
+
+// https://dev.to/_staticvoid/how-to-run-typescript-natively-in-nodejs-with-tsx-3a0c
+// https://tsx.is/node/esm#command-line-api
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -20,21 +24,32 @@ const pathsToExclude = (await readdir(audioPath))
  * 
  * @param {String[]} paths 
  */
-async function excludeFilesFromBuild(paths) {
+async function excludeFilesFromBuild(paths: string[]) {
     console.log(crayon.lightYellow('Removing the following paths from build output:'))
     paths.forEach(async (src) => {
         const resolvedPathName = path.join(__dirname, src)
         const parentDirectory = path.basename(path.dirname(resolvedPathName))
         const file = path.basename(resolvedPathName)
         // console.log(`${path.join(__dirname, '..')}${crayon.lightRed(`/${audioPath}/${file}`)}`)
-        console.log(`${audioPath}/${crayon.lightRed(`${file}`)}`)
+        console.log(`   ${audioPath}/${crayon.lightRed(`${file}`)}`)
         await rm(resolvedPathName, { recursive: true, force: true })
     })
 }
 
+async function updatePWAManifest() {
+    const start_url = '/outside-the-suitcase'
+    console.log(`${crayon.lightYellow('Changing manifest.json for production:')}`)
+    console.log(`   start_url: ${crayon.lightGreen(start_url)}`)
+    manifest.start_url = start_url
+    await writeFile(path.join(__dirname, '../build/manifest.json'), JSON.stringify(manifest, null, 4), { encoding: 'utf8' })
+}
+
 (async () => {
-    Promise.all([excludeFilesFromBuild(pathsToExclude)])
-        .catch((error) => {
-            console.log(error)
-        })
+    Promise.all([
+        excludeFilesFromBuild(pathsToExclude),
+        updatePWAManifest()
+    ])
+    .catch((error) => {
+        console.log(error)
+    })
 })()
